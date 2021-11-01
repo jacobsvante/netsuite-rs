8cc4ceff61fa3a0127642d2a7fc59bfd70a08902
Jacob Magnusson <m@jacobian.se>
2021-11-01 11:05:33 +0100

    fix: Include error message on IO error

--- src/error.rs
+++ src/error.rs
@@ -4,7 +4,7 @@ pub enum Error {
     HttpRequestError(u16, String),
     #[error("{0}")]
     HttpTransportError(String),
-    #[error("IO error")]
+    #[error("IO error: {0}")]
     IOError(#[from] std::io::Error),
     #[error("Serde error")]
     SerializationError(#[from] serde_json::Error),
